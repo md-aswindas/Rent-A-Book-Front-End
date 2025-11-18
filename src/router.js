e9fc69a20fe7ba9ea9c30393b18de6512f1f5ab0
md-aswindas <md.aswindas@gmail.com>
2025-11-18 11:49:27 +0530

    Library-Login & Registration

--- src/router.js
+++ src/router.js
@@ -4,7 +4,9 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/AdminRegistration', component: () => import('./components/Admin/AdminRegister.vue') },
-     { path: '/AdminLogin', component: () => import('./components/Admin/AdminLogin.vue') },
+    { path: '/AdminLogin', component: () => import('./components/Admin/AdminLogin.vue') },
+    { path: '/LibraryLogin', component: () => import('./components/Library/LibraryLogin.vue') },
+    { path: '/LibraryRegistration', component: () => import('./components/Library/LibraryRegistration.vue') },
   ] })
 
 export default router;
